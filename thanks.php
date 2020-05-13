a84203a961eb10b194c02182f4c5e17bff73c846
SebastienLeGal <64834388+SebastienLeGal@users.noreply.github.com>
2020-05-13 17:40:02 +0200

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,6 @@
+<?php echo "Merci "; require $surname; require $name; de nous avoir contacté à propos de require $Sujet; ?> .
+<? Un de nos conseiller vous contactera soit à l’adresse require $mail;
+<? ou par téléphone au require $tel; dans les plus brefs délais pour traiter votre demande :
+<? require $Message; ?>
+?>
+ 
