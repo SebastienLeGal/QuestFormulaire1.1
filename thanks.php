ca851daadeb3f6279dac7183678b1421a1ef51d6
SebastienLeGal <64834388+SebastienLeGal@users.noreply.github.com>
2020-05-25 09:34:30 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,11 +1,24 @@
-<?php 
-$prenom=$_GET['prenom'];
-$name=$_GET['name'];
-$mail=$_GET['mail'];
-$tel=$_GET['tel'];
-$Sujet=$_GET["Sujet"];
-$message=$_GET["message"]
-?>
-<h1>Merci, <?php echo $_GET["prenom"]. $_GET["name"] ?></h1> <h2>nous avoir contacté à propos de : <?php echo $_GET['Sujet] ?></h2>
-<h3>Un de nos conseiller vous contactera soit à l’adresse <?php echo $_GET["mail"] ?></h3>
-<h4>ou par téléphone au : <?php echo $_GET["tel"] ?></h4> <h5>dans les plus brefs délais pour traiter votre demande : <?php echo $_GET["message"] ?></h5>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page de traitement</title>
+</head>
+
+<p>
+    <?php
+
+    echo 'Merci ' .$_POST["prenom"].' '.$_POST["nom"].' de nous avoir contacté à propos de "'.$_POST["sujet"].'".';
+    echo'<br><br>';
+    echo 'Un de nos conseiller vous contactera soit à l’adresse '.$_POST["email"].' ou par téléphone au 
+    '.$_POST["telephone"].' dans les plus brefs délais pour traiter votre demande:';
+    echo'<br><br>';
+    echo $_POST["message"];
+    ?>
+
+    
+</p>
+
+
+
+</html>
