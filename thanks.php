2a89c69ec49dee96a41b91fd774bed03c6e51691
SebastienLeGal <64834388+SebastienLeGal@users.noreply.github.com>
2020-05-25 08:41:40 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,6 +1,11 @@
-<?php echo "Merci "; require $surname; require $name; de nous avoir contacté à propos de require $Sujet; ?> .
-<? Un de nos conseiller vous contactera soit à l’adresse require $mail;
-<? ou par téléphone au require $tel; dans les plus brefs délais pour traiter votre demande :
-<? require $Message; ?>
+<?php 
+$prenom=$_GET['prenom'];
+$name=$_GET['name'];
+$mail=$_GET['mail'];
+$tel=$_GET['tel'];
+$Sujet=$_GET["Sujet"];
+$message=$_GET["message"]
 ?>
- 
+<h1>Merci, <?php echo $_GET["prenom"]. $_GET["name"] ?></h1> <h2>nous avoir contacté à propos de : <?php echo $_GET['Sujet] ?></h2>
+<h3>Un de nos conseiller vous contactera soit à l’adresse <?php echo $_GET["mail"] ?></h3>
+<h4>ou par téléphone au : <?php echo $_GET["tel"] ?></h4> <h5>dans les plus brefs délais pour traiter votre demande : <?php echo $_GET["message"] ?></h5>
