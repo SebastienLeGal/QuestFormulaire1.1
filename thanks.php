73c1a4553784c0f38152cdb84aa206ed2b878fab
SebastienLeGal <64834388+SebastienLeGal@users.noreply.github.com>
2020-06-04 16:19:32 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -7,7 +7,7 @@
 
 <p>
     <?php
-        echo "Merci $prenom $nom de nous avoir contacté à propos de $sujet.
+        echo "Merci $firstName $name de nous avoir contacté à propos de $subjet.
         Un de nos conseiller vous contactera soit à l’adresse $email ou par téléphone au
         $tel dans les plus brefs délais pour traiter votre demande : $message";
     ?>
